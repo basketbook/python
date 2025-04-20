cac86c236f57c2b885db42540437f252d954bc37
Luke M <funie@outlook.com.br>
2025-04-20 10:50:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 #Variables
 name = "Luke"
 print(name)
+
+#List type of variable
+name = "Luke"
+print(type(name))
+
+#Strings
+a="hole"
+b="fire in the"
+
+#Two Integer
+a=1
+b=2
+c=47474747474
+
+#Float
+a=1.3
